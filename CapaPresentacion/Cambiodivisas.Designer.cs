65bd4b5406a3af53a5bd6d47e6ce8e18247b5a5a
kenrich paiz <kenrrichggmail.com>
2025-01-25 15:02:21 -0600

    form mesa  de cambio

---
+++ CapaPresentacion/Cambiodivisas.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace CapaPresentacion
+{
+    partial class Cambiodivisas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Cambiodivisas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(855, 489);
+            this.MaximizeBox = false;
+            this.Name = "Cambiodivisas";
+            this.Opacity = 0.5D;
+            this.Padding = new System.Windows.Forms.Padding(8);
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
