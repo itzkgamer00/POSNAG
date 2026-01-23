b1a6df4df7477f9d88b0603549af865776578b9b
kenrich paiz <kenrrichggmail.com>
2026-01-23 15:11:30 -0600

    confirmacion

--- CapaPresentacion/Formularios/FrmIngreso.Designer.cs
+++ CapaPresentacion/Formularios/FrmIngreso.Designer.cs
@@ -54,9 +54,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft JhengHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 99);
+            this.label1.Location = new System.Drawing.Point(14, 124);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(185, 36);
+            this.label1.Size = new System.Drawing.Size(216, 42);
             this.label1.TabIndex = 0;
             this.label1.Text = "OPERACION";
             // 
@@ -64,27 +64,29 @@
             // 
             this.comboBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(18, 138);
+            this.comboBox1.Location = new System.Drawing.Point(20, 172);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(665, 43);
+            this.comboBox1.Size = new System.Drawing.Size(748, 49);
             this.comboBox1.TabIndex = 1;
             // 
             // comboBox2
             // 
             this.comboBox2.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(18, 235);
+            this.comboBox2.Location = new System.Drawing.Point(20, 294);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(284, 43);
+            this.comboBox2.Size = new System.Drawing.Size(319, 49);
             this.comboBox2.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 196);
+            this.label2.Location = new System.Drawing.Point(14, 245);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(144, 36);
+            this.label2.Size = new System.Drawing.Size(171, 43);
             this.label2.TabIndex = 2;
             this.label2.Text = "MONEDA";
             // 
@@ -92,56 +94,59 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(393, 196);
+            this.label3.Location = new System.Drawing.Point(442, 245);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(127, 36);
+            this.label3.Size = new System.Drawing.Size(151, 43);
             this.label3.TabIndex = 4;
             this.label3.Text = "MONTO";
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(399, 235);
+            this.textBox1.Location = new System.Drawing.Point(449, 294);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(284, 43);
+            this.textBox1.Size = new System.Drawing.Size(319, 53);
             this.textBox1.TabIndex = 5;
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(18, 454);
+            this.textBox2.Location = new System.Drawing.Point(20, 568);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(284, 43);
+            this.textBox2.Size = new System.Drawing.Size(319, 53);
             this.textBox2.TabIndex = 8;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(12, 415);
+            this.label4.Location = new System.Drawing.Point(14, 519);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(181, 36);
+            this.label4.Size = new System.Drawing.Size(217, 43);
             this.label4.TabIndex = 7;
             this.label4.Text = "REFERENCIA";
             // 
             // textBox3
             // 
             this.textBox3.Font = new System.Drawing.Font("Microsoft JhengHei UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(18, 554);
+            this.textBox3.Location = new System.Drawing.Point(20, 686);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(665, 98);
+            this.textBox3.Size = new System.Drawing.Size(748, 73);
             this.textBox3.TabIndex = 10;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(12, 511);
+            this.label5.Location = new System.Drawing.Point(14, 639);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(202, 36);
+            this.label5.Size = new System.Drawing.Size(241, 43);
             this.label5.TabIndex = 9;
             this.label5.Text = "DESCRIPCION";
             // 
@@ -152,8 +157,9 @@
             this.panel1.Controls.Add(this.label11);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(727, 82);
+            this.panel1.Size = new System.Drawing.Size(818, 102);
             this.panel1.TabIndex = 43;
             // 
             // label11
@@ -163,10 +169,10 @@
             this.label11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label11.Font = new System.Drawing.Font("Bahnschrift", 19.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label11.ForeColor = System.Drawing.SystemColors.InfoText;
-            this.label11.Location = new System.Drawing.Point(226, 7);
+            this.label11.Location = new System.Drawing.Point(254, 9);
             this.label11.Name = "label11";
             this.label11.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.label11.Size = new System.Drawing.Size(347, 50);
+            this.label11.Size = new System.Drawing.Size(390, 62);
             this.label11.TabIndex = 4;
             this.label11.Text = "INGRESO";
             this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -184,9 +190,10 @@
             this.iconButton2.IconColor = System.Drawing.Color.Red;
             this.iconButton2.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.iconButton2.IconSize = 30;
-            this.iconButton2.Location = new System.Drawing.Point(18, 685);
+            this.iconButton2.Location = new System.Drawing.Point(20, 773);
+            this.iconButton2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.iconButton2.Name = "iconButton2";
-            this.iconButton2.Size = new System.Drawing.Size(196, 57);
+            this.iconButton2.Size = new System.Drawing.Size(220, 71);
             this.iconButton2.TabIndex = 13;
             this.iconButton2.Text = "CANCELAR";
             this.iconButton2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
@@ -204,9 +211,10 @@
             this.btnguardaringreso.IconColor = System.Drawing.Color.Green;
             this.btnguardaringreso.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.btnguardaringreso.IconSize = 30;
-            this.btnguardaringreso.Location = new System.Drawing.Point(497, 685);
+            this.btnguardaringreso.Location = new System.Drawing.Point(563, 773);
+            this.btnguardaringreso.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnguardaringreso.Name = "btnguardaringreso";
-            this.btnguardaringreso.Size = new System.Drawing.Size(182, 57);
+            this.btnguardaringreso.Size = new System.Drawing.Size(205, 71);
             this.btnguardaringreso.TabIndex = 12;
             this.btnguardaringreso.Text = "GUARDAR";
             this.btnguardaringreso.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
@@ -217,9 +225,10 @@
             this.btndetalleingr.IconChar = FontAwesome.Sharp.IconChar.Table;
             this.btndetalleingr.IconColor = System.Drawing.Color.Black;
             this.btndetalleingr.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.btndetalleingr.Location = new System.Drawing.Point(308, 344);
+            this.btndetalleingr.Location = new System.Drawing.Point(346, 430);
+            this.btndetalleingr.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btndetalleingr.Name = "btndetalleingr";
-            this.btndetalleingr.Size = new System.Drawing.Size(67, 43);
+            this.btndetalleingr.Size = new System.Drawing.Size(75, 54);
             this.btndetalleingr.TabIndex = 6;
             this.btndetalleingr.UseVisualStyleBackColor = true;
             this.btndetalleingr.Click += new System.EventHandler(this.btndetalleingr_Click);
@@ -227,19 +236,20 @@
             // textBox4
             // 
             this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.Location = new System.Drawing.Point(18, 344);
+            this.textBox4.Location = new System.Drawing.Point(20, 430);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox4.Multiline = true;
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(284, 43);
+            this.textBox4.Size = new System.Drawing.Size(319, 53);
             this.textBox4.TabIndex = 45;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(12, 305);
+            this.label6.Location = new System.Drawing.Point(14, 381);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(277, 36);
+            this.label6.Size = new System.Drawing.Size(332, 43);
             this.label6.TabIndex = 44;
             this.label6.Text = "IMPORTE RECIBIDO";
             // 
@@ -247,28 +257,29 @@
             // 
             this.textBox5.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox5.Location = new System.Drawing.Point(399, 344);
+            this.textBox5.Location = new System.Drawing.Point(449, 430);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox5.Multiline = true;
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(284, 43);
+            this.textBox5.Size = new System.Drawing.Size(319, 53);
             this.textBox5.TabIndex = 47;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(393, 305);
+            this.label7.Location = new System.Drawing.Point(442, 381);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(277, 36);
+            this.label7.Size = new System.Drawing.Size(331, 43);
             this.label7.TabIndex = 46;
             this.label7.Text = "ENTREGAR VUELTO";
             // 
             // FrmIngreso
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.ClientSize = new System.Drawing.Size(727, 785);
+            this.ClientSize = new System.Drawing.Size(818, 852);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.textBox4);
@@ -288,6 +299,7 @@
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FrmIngreso";
